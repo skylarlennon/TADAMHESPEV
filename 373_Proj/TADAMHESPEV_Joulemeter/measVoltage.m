0037933be50036caee6f8da6829ae04d29e84a9e
Skylar <skylarl@umich.edu>
2023-11-15 09:55:36 -0500

    [unfinished] power consumption for voltage meas

--- 373_Proj/TADAMHESPEV_Joulemeter/measVoltage.m
+++ 373_Proj/TADAMHESPEV_Joulemeter/measVoltage.m
@@ -11,6 +11,7 @@ syms r1 r2
 
 Vmin = 46;                  %Volts
 Vmax = 54;                  %Volts
+V_array = linspace(Vmin,Vmax,1000);
 ADC_Bits = 12;
 Vref_ADC = 3.3;             %Volts
 maxPowerVoltMeter = 0.25;   %Watts
@@ -25,9 +26,15 @@ a = solve([peqn vd_eqn], [r1 r2]);
 r1Val = a.r1;
 r2Val = a.r2;
 
+R_array = linspace(0,r1Val+r2Val+2000,1000);
+
+[V_AR,R_AR] = meshgrid(V_array,R_array);
+
 VoltageRange = (Vmax*r1Val/(r1Val + r2Val)) - (Vmin*r1Val/(r1Val + r2Val));
-numADCVals = vpa((2^12/Vref_ADC) * VoltageRange, 4)
-adcResolution = vpa(VoltageRange / numADCVals, 4) %volts/adcVal
+numADCVals = vpa((2^12/Vref_ADC) * VoltageRange, 4);
+adcResolution = vpa(VoltageRange / numADCVals, 4); %volts/adcVal
+
+
 
 
 
