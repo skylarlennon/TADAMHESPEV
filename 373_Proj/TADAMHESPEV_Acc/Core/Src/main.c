febb54f92504c07bfb131ebf366d5d7b7207fecd
Skylar <skylarl@umich.edu>
2023-11-17 20:52:43 -0500

    [added] test for GPIO_WRITE

--- 373_Proj/TADAMHESPEV_Acc/Core/Src/main.c
+++ 373_Proj/TADAMHESPEV_Acc/Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdio.h"
+#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -86,38 +87,6 @@ void setupAccModule(){
 	}
 }
 
-float ReadAccData(){
-		uint8_t buf[1]= {OUT_X_L_A | 1 << 7}; //Auto-Increment OUT_X_L_A
-		uint8_t rbuf[2];
-		char axischars[3] = {'x', 'z', 'y'};
-		static int cnt = 0;
-		float accVal = 0;
-
-		HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(&hi2c1, ACC_I2C_ADDR << 1, buf, 1, 1000);
-		//[TODO] - Have error checking for communication errors
-		ret =  HAL_I2C_Master_Receive(&hi2c1, ACC_I2C_ADDR << 1, rbuf, 2, 1000);
-		//[TODO] - Have error checking for communication errors
-
-		uint16_t raw = (rbuf[1] << 8) | rbuf[0];	// 2's compliment, +-2g's
-		if(raw > 64100){
-			accVal = 0;
-		}
-		else if(raw & 0x8000){ //if value is negative
-			int16_t temp = -((raw ^ 0xFFFF) + 1);
-			accVal = (temp / (float)(1 << 15))*2;
-		}
-		else{ //positive acceleration
-			accVal = (raw / (float)((1 << 15) - 1))*2;
-		}
-		return accVal;
-//		uint8_t binAcc = accFloat2Binary(accVal);
-		//[TODO] - error check if binAcc = -1;
-
-//		printf("%i\t%b\n",binAcc);
-
-//		printf("%i\t%f\n",cnt++, accVal);
-}
-
 uint8_t accFloat2Binary(float accVal){
 	if(fabs(accVal) < 0.125){
 		return 0;
@@ -172,6 +141,88 @@ uint8_t accFloat2Binary(float accVal){
 	}
 }
 
+void printBinaryNewline(uint8_t accBin){
+	int size = sizeof(accBin)*8;
+	for(int i = size - 1; i >= 0; --i){
+		printf("%i",(accBin >> i)&1);
+	}
+	printf("\n");
+}
+
+void printBinary(uint8_t accBin){
+	int size = sizeof(accBin)*8;
+	for(int i = size - 1; i >= 0; --i){
+		printf("%i",(accBin >> i)&1);
+	}
+}
+
+void printAll(uint16_t raw, float acc, uint8_t accBin){
+	printf("Raw:\t%u\tAcc:\t%f\tBinary:\t",raw,acc);
+	printBinary(accBin);
+	printf("\n");
+}
+
+float ReadAccData(){
+		uint8_t buf[1]= {OUT_X_L_A | 1 << 7}; //Auto-Increment OUT_X_L_A
+		uint8_t rbuf[2];
+		float accVal = 0;
+
+		HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(&hi2c1, ACC_I2C_ADDR << 1, buf, 1, 1000);
+		//[TODO] - Have error checking for communication errors
+		ret =  HAL_I2C_Master_Receive(&hi2c1, ACC_I2C_ADDR << 1, rbuf, 2, 1000);
+		//[TODO] - Have error checking for communication errors
+
+		uint16_t raw = (rbuf[1] << 8) | rbuf[0];	// 2's compliment, +-2g's
+		if(raw > 64100){
+			accVal = 0;
+		}
+		else if(raw & 0x8000){ //if value is negative
+			int16_t temp = -((raw ^ 0xFFFF) + 1);
+			accVal = (temp / (float)(1 << 15))*2;
+		}
+		else{ //positive acceleration
+			accVal = (raw / (float)((1 << 15) - 1))*2;
+		}
+		return accVal;
+//		uint8_t binAcc = accFloat2Binary(accVal);
+		//[TODO] - error check if binAcc = -1;
+
+//		printf("%i\t%b\n",binAcc);
+
+//		printf("%i\t%f\n",cnt++, accVal);
+}
+
+void print_raw_acc_bin(){
+		uint8_t buf[1]= {OUT_X_L_A | 1 << 7}; //Auto-Increment OUT_X_L_A
+		uint8_t rbuf[2];
+		float accVal = 0;
+
+		HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(&hi2c1, ACC_I2C_ADDR << 1, buf, 1, 1000);
+		//[TODO] - Have error checking for communication errors
+		ret =  HAL_I2C_Master_Receive(&hi2c1, ACC_I2C_ADDR << 1, rbuf, 2, 1000);
+		//[TODO] - Have error checking for communication errors
+
+		uint16_t raw = (rbuf[1] << 8) | rbuf[0];	// 2's compliment, +-2g's
+		if(raw > 64100){
+			accVal = 0;
+		}
+		else if(raw & 0x8000){ //if value is negative
+			int16_t temp = -((raw ^ 0xFFFF) + 1);
+			accVal = (temp / (float)(1 << 15))*2;
+		}
+		else{ //positive acceleration
+			accVal = (raw / (float)((1 << 15) - 1))*2;
+		}
+		uint8_t accBin = accFloat2Binary(accVal);
+		printAll(raw,accVal,accBin);
+}
+
+
+
+
+
+
+
 void write4BitGPIOs(uint8_t binVal){
 //	HAL_GPIO_WritePin(GPIOX, GPIO_PIN_XX,  pin_state);
 
@@ -179,8 +230,84 @@ void write4BitGPIOs(uint8_t binVal){
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, (binVal >> 3) & 1);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, (binVal >> 2) & 1);
 	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, (binVal >> 1) & 1);
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, binVal & 1);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, binVal & 1);
 }
+
+void writeGPIOCountTest(){
+	// 0
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,0); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,0); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,0); // ACC[0]
+
+	HAL_Delay(500);
+
+	//1
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,0); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,0); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,1); // ACC[0]
+
+	HAL_Delay(500);
+
+	//2
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,0); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,1); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,0); // ACC[0]
+
+	HAL_Delay(500);
+
+	//3
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,0); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,1); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,1); // ACC[0]
+
+	HAL_Delay(500);
+
+	//4
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,1); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,0); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,0); // ACC[0]
+
+	HAL_Delay(500);
+
+	//5
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,1); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,0); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,1); // ACC[0]
+
+	HAL_Delay(500);
+
+	//6
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,1); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,1); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,0); // ACC[0]
+
+	HAL_Delay(500);
+
+	//7
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,0); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,1); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,1); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,1); // ACC[0]
+
+	HAL_Delay(500);
+
+	//8
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6,1); // ACC[3]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5,0); // ACC[2]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4,0); // ACC[1]
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3,0); // ACC[0]
+
+	HAL_Delay(500);
+}
+
+
 
 /* USER CODE END 0 */
 
@@ -219,13 +346,18 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+//  static int cnt = 0;
   setupAccModule();
   while (1)
   {
 	 float acc = ReadAccData();
 	 uint8_t binAcc = accFloat2Binary(acc);
+	 printBinaryNewline(binAcc);
 	 write4BitGPIOs(binAcc);
 	 HAL_Delay(10);
+
+
+//	writeGPIOCountTest();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
